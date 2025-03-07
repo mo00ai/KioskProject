065b6431b65185d9f812673644603a14abb1267b
AhyunCho <jah112482@gmail.com>
2025-03-07 22:51:59 +0900

    inserBurgers method create

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -1,9 +1,43 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kiosk {
+    MenuItem menuItem;
+
     //프로그램 순서 및 흐름 제어를 담당하는 클래스
+    private List<MenuItem> menuItems;
+
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public void insertBurgers() {
+
+        MenuItem menuItem1 = new MenuItem("싸이버거", 4.9,"매콤한 통다리살 패티가 통째로~ 맘스터치 시그니처 버거");
+        MenuItem menuItem2 =new MenuItem("불싸이버거", 5.1,"불 맛이 살아있는 싸이버거의 진또배기");
+        MenuItem menuItem3 = new MenuItem("화이트갈릭버거", 4.9,"크림처럼 부드러운 화이트갈릭 소스에 통가슴살");
+        MenuItem menuItem4 = new MenuItem("통새우버거", 4.9,"통새우살 패티에 신선한 양상추는 덤~");
+        MenuItem menuItem5 = new MenuItem("딥치즈버거", 4.9,"부드러운 치즈와 한층 더 촉촉해진 닭가슴살");
+
+        Kiosk kiosk = new Kiosk(List.of(menuItem1, menuItem2, menuItem3,menuItem4,menuItem5));
+
+    }
+
+    public void start() {
+
+        insertBurgers();
 
 
 
-
+    }
 }
--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -32,19 +32,22 @@ public class Main {
 
         //list 선언 및 초기화
         //add 함수를 통해 new MenuOtem(이름,가격,설명) list 추가
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(new MenuItem("싸이버거", 4.9,"매콤한 통다리살 패티가 통째로~ 맘스터치 시그니처 버거"));
-        menuItems.add(new MenuItem("불싸이버거", 5.1,"불 맛이 살아있는 싸이버거의 진또배기"));
-        menuItems.add(new MenuItem("화이트갈릭버거", 4.9,"크림처럼 부드러운 화이트갈릭 소스에 통가슴살"));
-        menuItems.add(new MenuItem("통새우버거", 4.9,"통새우살 패티에 신선한 양상추는 덤~"));
-        menuItems.add(new MenuItem("딥치즈버거", 4.9,"부드러운 치즈와 한층 더 촉촉해진 닭가슴살"));
 
+//        menuItems.add(new MenuItem("싸이버거", 4.9,"매콤한 통다리살 패티가 통째로~ 맘스터치 시그니처 버거"));
+//        menuItems.add(new MenuItem("불싸이버거", 5.1,"불 맛이 살아있는 싸이버거의 진또배기"));
+//        menuItems.add(new MenuItem("화이트갈릭버거", 4.9,"크림처럼 부드러운 화이트갈릭 소스에 통가슴살"));
+//        menuItems.add(new MenuItem("통새우버거", 4.9,"통새우살 패티에 신선한 양상추는 덤~"));
+//        menuItems.add(new MenuItem("딥치즈버거", 4.9,"부드러운 치즈와 한층 더 촉촉해진 닭가슴살"));
 
+        MenuItem menuItem1 = new MenuItem("싸이버거", 4.9,"매콤한 통다리살 패티가 통째로~ 맘스터치 시그니처 버거");
+        MenuItem menuItem2 =new MenuItem("불싸이버거", 5.1,"불 맛이 살아있는 싸이버거의 진또배기");
+        MenuItem menuItem3 = new MenuItem("화이트갈릭버거", 4.9,"크림처럼 부드러운 화이트갈릭 소스에 통가슴살");
+        MenuItem menuItem4 = new MenuItem("통새우버거", 4.9,"통새우살 패티에 신선한 양상추는 덤~");
+        MenuItem menuItem5 = new MenuItem("딥치즈버거", 4.9,"부드러운 치즈와 한층 더 촉촉해진 닭가슴살");
+
+        Kiosk kiosk = new Kiosk(List.of(menuItem1, menuItem2, menuItem3,menuItem4,menuItem5));
 
         Scanner scanner = new Scanner(System.in);
-
-
-
 
         //메뉴 고르기
         while(true) {
@@ -53,11 +56,11 @@ public class Main {
                 //메뉴들 출력
                 System.out.println(" 🍔 맘스터치 메뉴 🍔 ");
 
-                for(int i = 0; i<menuItems.size(); i++) {
+                for(int i = 0; i<kiosk.getMenuItems().size(); i++) {
 
-                    String menuName = menuItems.get(i).getMenuName();
-                    double menuPrice = menuItems.get(i).getMenuPrice();
-                    String menuInfo = menuItems.get(i).getMenuInfo();
+                    String menuName = kiosk.getMenuItems().get(i).getMenuName();
+                    double menuPrice = kiosk.getMenuItems().get(i).getMenuPrice();
+                    String menuInfo = kiosk.getMenuItems().get(i).getMenuInfo();
 
                     //영어는 한 글자당 1byte
                     //한글은 한 글자당 2byte
@@ -73,20 +76,19 @@ public class Main {
                 System.out.println("0 . 종료               |  종료");
                 System.out.println();
 
-
                 System.out.print("1~5 또는 0을 입력하세요: ");
                 int num = scanner.nextInt();
 
                 if (num == 1) {
                     System.out.println("\n-------- 선택한 메뉴 --------");
-                    String menuName = menuItems.get(num-1).getMenuName();
-                    double menuPrice = menuItems.get(num-1).getMenuPrice();
-                    String menuInfo = menuItems.get(num-1).getMenuInfo();
+                    String menuName = kiosk.getMenuItems().get(num-1).getMenuName();
+                    double menuPrice = kiosk.getMenuItems().get(num-1).getMenuPrice();
+                    String menuInfo = kiosk.getMenuItems().get(num-1).getMenuInfo();
+
                     System.out.printf("%-2d. %-15s| W %5.1f | %s%n", num, menuName, menuPrice, menuInfo);
 
                     System.out.println("\n9. 이전으로");
                     System.out.println("0. 종료");
-
 
                     System.out.print("\n입력 : ");
                     num = scanner.nextInt();
